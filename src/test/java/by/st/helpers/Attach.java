ae8fe7bfff228850aa637d81d53ff16498341f75
VeronikaMonarkhovich <taory4cources@gmail.com>
2021-10-25 18:18:54 +0300

    init tests

--- src/test/java/by/st/helpers/Attach.java
+++ src/test/java/by/st/helpers/Attach.java
@@ -44,7 +44,7 @@ public class Attach {
     }
 
     public static URL getVideoUrl(String sessionId) {
-        String videoUrl = "http://localhost:4444/video/" + sessionId + ".mp4";
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId + ".mp4";
 
         try {
             return new URL(videoUrl);
--- src/test/java/by/st/tests/TestBase.java
+++ src/test/java/by/st/tests/TestBase.java
@@ -23,7 +23,7 @@ public class TestBase {
         capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
 
         Configuration.browserCapabilities = capabilities;
-        // Configuration.remote = "http://localhost:4444/wd/hub/";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
     @AfterEach
